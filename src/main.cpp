b9a356fa5c45002c47979d55b4e26cd218b41834
Eric Konks <eric.konks@gmail.com>
2025-08-17 18:13:34 +0300

    Add simple cube render

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <lvk/LVK.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
 #include "utils.h"
 
 static constexpr uint32_t WIDTH = 1680;
 static constexpr uint32_t HEIGHT = 720;
 
-static const char* LOG_FILE_PATH = ".log.last.txt";
+static constexpr uint32_t CUBE_TRIANGLES = 36;
 
-static const char* VERT_PATH = "src/shaders/triangle.vert";
-static const char* FRAG_PATH = "src/shaders/triangle.frag";
+static const char* LOG_FILE_PATH = ".log.last.txt"; 
+
+static const char* VERT_PATH = "src/shaders/cube.vert";
+static const char* FRAG_PATH = "src/shaders/cube.frag";
 
 class WindowApp final
 {
@@ -29,6 +34,15 @@ public:
             .smVert = vert,
             .smFrag = frag,
             .color  = { { .format = ctx->getSwapchainFormat() } },
+            .cullMode = lvk::CullMode_Back,
+        });
+        wireframePipeline = ctx->createRenderPipeline({
+            .smVert = vert,
+            .smFrag = frag,
+            .specInfo = { .entries = { { .constantId = 0, .size = sizeof(isWireframe) } }, .data = &isWireframe, .dataSize = sizeof(isWireframe) },
+            .color  = { { .format = ctx->getSwapchainFormat() } },
+            .cullMode = lvk::CullMode_Back,
+            .polygonMode = lvk::PolygonMode_Line,
         });
     }
 
@@ -45,6 +59,14 @@ public:
             if (!width || !height)
                 continue;
 
+            const float ratio = width / (float)height;
+
+            const glm::mat4 m = glm::rotate(
+                glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.5f)),
+                (float)glfwGetTime(),
+                glm::vec3(1.0f, 1.0f, 1.0f));
+            const glm::mat4 p = glm::perspective(45.0f, ratio, 0.1f, 1000.0f);
+
             lvk::ICommandBuffer& buf = ctx->acquireCommandBuffer();
 
             buf.cmdBeginRendering(
@@ -52,10 +74,21 @@ public:
                 { .color = { { .texture = ctx->getCurrentSwapchainTexture() } } }
             );
 
-            buf.cmdBindRenderPipeline(pipeline);
-            buf.cmdPushDebugGroupLabel("Render Triangle", 0xff0000ff);
-            buf.cmdDraw(3);
-            buf.cmdPopDebugGroupLabel();
+            {
+                buf.cmdPushDebugGroupLabel("Render cube", 0xff0000ff);
+                buf.cmdBindRenderPipeline(pipeline);
+                buf.cmdPushConstants(p * m);
+                buf.cmdDraw(CUBE_TRIANGLES);
+                buf.cmdPopDebugGroupLabel();
+            }
+
+            {
+                buf.cmdPushDebugGroupLabel("Render wireframe cube", 0xff0000ff);
+                buf.cmdBindRenderPipeline(wireframePipeline);
+                buf.cmdPushConstants(p * m);
+                buf.cmdDraw(CUBE_TRIANGLES);
+                buf.cmdPopDebugGroupLabel();
+            }
 
             buf.cmdEndRendering();
 
@@ -69,16 +102,20 @@ public:
         vert.reset();
         frag.reset();
         pipeline.reset();
+        wireframePipeline.reset();
         ctx.reset();
         glfwTerminate();
     }
 private:
-    std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window;
-    std::unique_ptr<lvk::IContext> ctx;
+    VkBool32 isWireframe = false;
 
     lvk::Holder<lvk::ShaderModuleHandle> vert;
     lvk::Holder<lvk::ShaderModuleHandle> frag;
     lvk::Holder<lvk::RenderPipelineHandle> pipeline;
+    lvk::Holder<lvk::RenderPipelineHandle> wireframePipeline;
+
+    std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window;
+    std::unique_ptr<lvk::IContext> ctx;
 };
 
 int main()
